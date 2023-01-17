43d718fd771194043db7df8a03a2de3ba9d1bc79
Tarkin25 <severin.weigold@gmx.ch>
2023-01-17 20:06:58 +0100

    start using noise

--- src/mountain.rs
+++ src/mountain.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use noise::{Perlin, NoiseFn, Terrace};
 
 use crate::heightmap::Heightmap;
 
@@ -7,14 +8,15 @@ pub struct MountainPlugin;
 impl Plugin for MountainPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_startup_system_to_stage(StartupStage::PreStartup, insert_noise_config)
             .add_startup_system(insert_plane);
     }
 }
 
-fn insert_plane(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
-    let mut heightmap = Heightmap::new(100.0, 1.0);
+fn insert_plane(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>, noise_config: Res<NoiseConfig>) {    
+    let mut heightmap = Heightmap::new(100.0, 0.5);
     heightmap.compute_heights(|x, z| {
-        x.sin() + z.sin()
+        noise_config.noise.get([x as f64 * 0.1, z as f64 * 0.1]) as f32 * 50.0
     });
     
     commands.spawn(PbrBundle {
@@ -22,4 +24,20 @@ fn insert_plane(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut ma
         material: materials.add(Color::PURPLE.into()),
         ..Default::default()
     });
+}
+
+#[derive(Resource)]
+pub struct NoiseConfig {
+    pub noise: Box<dyn NoiseFn<f64, 2> + Send + Sync>,
+}
+
+fn insert_noise_config(mut commands: Commands) {
+    let perlin = Perlin::default();
+    let terrace = Terrace::new(perlin)
+    .add_control_point(-1.0)
+    .add_control_point(-0.5)
+    .add_control_point(0.1)
+    .add_control_point(1.0);
+
+    commands.insert_resource(NoiseConfig { noise: Box::new(terrace) });
 }
