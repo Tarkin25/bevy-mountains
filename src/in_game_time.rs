393dada8271a05508025200e79889cdcb6dc2b29
Tarkin25 <severin.weigold@gmx.ch>
2023-04-02 19:29:36 +0200

    pause / unpause in-game time

--- src/in_game_time.rs
+++ src/in_game_time.rs
@@ -8,6 +8,7 @@ impl Plugin for InGameTimePlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(InGameTime::setup)
             .add_system(InGameTime::update)
+            .add_system(InGameTime::toggle)
             .add_system_set(SystemSet::on_exit(GameState::Running).with_system(InGameTime::pause))
             .add_system_set(
                 SystemSet::on_enter(GameState::Running).with_system(InGameTime::unpause),
@@ -33,5 +34,17 @@ impl InGameTime {
 
     fn unpause(mut in_game_time: ResMut<InGameTime>) {
         in_game_time.0.unpause();
+    }
+
+    fn toggle(mut in_game_time: ResMut<InGameTime>, input: Res<Input<KeyCode>>) {
+        if input.just_pressed(KeyCode::T) {
+            let relative_speed = if in_game_time.relative_speed() == 0.0 {
+                1.0
+            } else {
+                0.0
+            };
+
+            in_game_time.0.set_relative_speed(relative_speed);
+        }
     }
 }
